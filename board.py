f67bae9a77a0798f1c20198e16ac15c69eb22ffa
esker-luminous <77069944+esker-luminous@users.noreply.github.com>
2021-01-06 10:24:23 -0700

    Create board.py

---
+++ board.py
@@ -0,0 +1,7 @@
+board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+
+
+def display_board(board):
+    print(' 1 | 2 | 3       ' + board[0] + '  |  ' + board[1] + '  |  ' + board[2])
+    print(' 4 | 5 | 6       ' + board[3] + '  |  ' + board[4] + '  |  ' + board[5])
+    print(' 7 | 8 | 9       ' + board[6] + '  |  ' + board[7] + '  |  ' + board[8])
